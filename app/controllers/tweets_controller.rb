f0dee490ed13af90247d8c7d3cd03c7bc1796a5a
yujiro11040827 <71250516+yujiro11040827@users.noreply.github.com>
2021-01-19 17:54:40 +0900

    マイページ作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
   
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user)
   end
 
   def new
@@ -37,7 +37,7 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(:tweet).permit(:name, :author, :genre_id, :impressions)
+    params.require(:tweet).permit(:name, :author, :genre_id, :impressions).merge(user_id: current_user.id)
   end
 
   def set_tweet
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_075106) do
+ActiveRecord::Schema.define(version: 2021_01_19_081602) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_01_19_075106) do
     t.text "impressions", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
