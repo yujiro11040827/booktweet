31fb59f87b4b357c7fc3f239f7c6fe57665f8f2c
yujiro11040827 <71250516+yujiro11040827@users.noreply.github.com>
2021-02-09 22:01:03 +0900

    アクション実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,20 @@
 class LikesController < ApplicationController
+
+  before_action :prepare_tweet
+
+  def create
+    @like = current.user.likes.new(tweet_id: @tweet.id)
+    @like.save
+    @likes = Like.where(tweet_id: @tweet.id)
+  end
+
+  def destroy
+    @like = Like.find_by(tweet_id: @tweet.id, user_id: current_user.id).destroy
+    @likes = Like.where(tweet_id: @tweet.id)
+  end
+
+  private
+  def prepare_tweet
+    @tweet = Tweet.find(params[:tweet_id])
+  end
 end
